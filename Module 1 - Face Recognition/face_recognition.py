1497bf3a9a84530a5435dcc43899e7a449de8d51
Akila Mendis <akila@oneit.com.au>
2021-08-16 14:55:54 +0530

    Face detection - Step5

--- Module 1 - Face Recognition/face_recognition.py
+++ Module 1 - Face Recognition/face_recognition.py
@@ -4,11 +4,15 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+
 # Defining a function that will do the directions
 def detect(grey, frame):
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_grey = grey[y:y+h, x:x+w]
+        roi_grey = grey[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_grey, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    return frame
