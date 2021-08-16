2e9021d671084d42b388894d03aa171e24a2cbca
Akila Mendis <akila@oneit.com.au>
2021-08-16 13:44:21 +0530

    Face detection - Step2

--- Module 1 - Face Recognition/face_recognition.py
+++ Module 1 - Face Recognition/face_recognition.py
@@ -2,4 +2,8 @@ import cv2
 
 # Loading the Cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Defining a function that will do the directions
+def detect(grey, frame):
+    faces = face_cascade.detectMultiScale(grey, 1.3, 5)
