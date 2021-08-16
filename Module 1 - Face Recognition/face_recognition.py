088a127d7a86aa86263c5904b47c2b2eec69a278
Akila Mendis <akila@oneit.com.au>
2021-08-16 14:18:17 +0530

    Face detection - Step4

--- Module 1 - Face Recognition/face_recognition.py
+++ Module 1 - Face Recognition/face_recognition.py
@@ -9,3 +9,6 @@ def detect(grey, frame):
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_grey = grey[y:y+h, x:x+w]
+        roi_color = frame[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_grey, 1.1, 3)
