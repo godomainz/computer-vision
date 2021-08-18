1bdf914c2803973c57a34c5f19c35c64ed26a345
Akila Mendis <akila@oneit.com.au>
2021-08-18 11:20:20 +0530

    Object Detection - Step 3

--- Module 2 - Object Detection/object_detection.py
+++ Module 2 - Object Detection/object_detection.py
@@ -5,4 +5,8 @@ from data import BaseTransform, VOC_CLASSES as labelmap
 from ssd import build_ssd
 import imageio
 
+
 # Defining a function that will do the detections
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    
