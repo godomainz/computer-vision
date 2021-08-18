568feb25b3312f29c8881744e3acff9042625f98
Akila Mendis <akila@oneit.com.au>
2021-08-18 13:23:05 +0530

    Object Detection - Step 6

--- Module 2 - Object Detection/object_detection.py
+++ Module 2 - Object Detection/object_detection.py
@@ -14,4 +14,14 @@ def detect(frame, net, transform):
     x = Variable(x.unsqueeze(0))
     y = net(x)
     detections = y.data
-    scale = torch.tensor(width, height, width, height)
+    scale = torch.tensor([width, height, width, height])
+    # detections = [batch, number_of_classes, number_of_occurrence, (score, x0, y0, x1, y1)]
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] > 0.6:
+            pt = (detections[0, i, j, 1:] * scale).numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
+
+    return frame
